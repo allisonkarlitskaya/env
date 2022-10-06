40dcae356dc8f4a17dc5e97dc979fa6630ac1490
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2022-10-06 11:26:31 +0200

    Migrate user dconf config to system level

---
+++ system/steps/dconf.sh
@@ -0,0 +1,2 @@
+cp -vt /etc/dconf/db/local.d etc/dconf/db/local.d/*
+dconf compile /etc/dconf/db/local /etc/dconf/db/local.d
--- user/steps/gnome.sh
+++ user/steps/gnome.sh
@@ -1,31 +1,3 @@
-dconf load / <<EOF
-[org/gnome/software]
-download-updates=false
-download-updates-notify=false
-first-run=false
-
-[org/gnome/desktop/search-providers]
-disabled=['org.gnome.Software.desktop']
-
-[org/gnome/desktop/input-sources]
-per-window=false
-sources=[('xkb', 'us+euro')]
-xkb-options=['lv3:ralt_switch', 'compose:caps', 'esperanto:qwerty']
-
-[org/gnome/desktop/wm/keybindings]
-close=['<Super>w']
-switch-applications=@as []
-switch-applications-backward=@as []
-switch-windows=['<Alt>Tab']
-switch-windows-backward=['<Shift><Alt>Tab']
-
-[org/gnome/desktop/interface]
-clock-show-weekday=true
-
-[org/gnome/desktop/calendar]
-show-weekdate=true
-EOF
-
 if ! gnome-extensions list | grep -q Move_Clock@rmy.pobox.com; then
     busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions \
         org.gnome.Shell.Extensions InstallRemoteExtension s 'Move_Clock@rmy.pobox.com'
